35ddab5bebc38b08d8677d0cc664891e13d3f0f3
Muzammil Ansaari <muzammilansaari6@gmail.com>
2025-07-25 21:22:02 +0500

    Backend Start

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,9 +4,11 @@ import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button';
 import { ArrowLeft, CarFront, Heart } from 'lucide-react';
+import { checkUser } from '@/lib/checkUser';
 
 const Header = async ({ isAdminPage = false }) => {
-  const isAdmin = false;
+  const user = await checkUser();
+  const isAdmin = user?.role === "ADMIN";
   return <header className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
     <nav className='flex items-center justify-between mx-auto py-2 px-4'>
       <Link href={isAdminPage ? "/admin" : "/"} className='flex'>
